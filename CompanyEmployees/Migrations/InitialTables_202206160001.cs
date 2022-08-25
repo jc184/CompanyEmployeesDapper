b1754292a9c414924dd6a92d8f66191fd2811f56
james chalmers <james.chalmers184@gmail.com>
2022-08-25 13:45:29 +0100

    Add project files.

---
+++ CompanyEmployees/Migrations/InitialTables_202206160001.cs
@@ -0,0 +1,35 @@
+﻿using FluentMigrator;
+
+namespace CompanyEmployees.Migrations
+{
+    [Migration(202206160001)]
+    public class InitialTables_202206160001 : Migration
+    {
+        public override void Down()
+        {
+            Delete.Table("Employees");
+            Delete.Table("Companies");
+        }
+        public override void Up()
+        {
+            Create.Table("Companies")
+            .WithColumn("CompanyId").AsGuid().NotNullable()
+            .PrimaryKey().WithDefaultValue("NEWID()")
+            .WithColumn("Name").AsString(50).NotNullable()
+            .WithColumn("Address").AsString(60).NotNullable()
+            .WithColumn("Country").AsString(50).NotNullable();
+            Create.Table("Employees")
+            .WithColumn("EmployeeId").AsGuid().NotNullable()
+            .PrimaryKey().WithDefaultValue("NEWID()")
+            .WithColumn("Name").AsString(50).NotNullable()
+            .WithColumn("Age").AsInt32().NotNullable()
+            .WithColumn("Position").AsString(50).NotNullable()
+            .WithColumn("CompanyId").AsGuid().NotNullable()
+            .ForeignKey("Companies", "CompanyId")
+            .OnDelete(System.Data.Rule.Cascade);
+        }
+
+
+    }
+
+}
---
+++ CompanyEmployees/Program.cs
@@ -0,0 +1,46 @@
+using CompanyEmployees.Extensions;
+using CompanyEmployees.Migrations;
+using Contracts;
+using Microsoft.AspNetCore.HttpOverrides;
+using NLog;
+using Repository;
+
+var builder = WebApplication.CreateBuilder(args);
+
+LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+builder.Services.ConfigureLoggerService();
+
+builder.Services.AddControllers()
+	.AddApplicationPart(typeof(CompanyEmployees.Presentation.AssemblyReference).Assembly);
+
+builder.Services.AddSingleton<DapperContext>();
+builder.Services.AddSingleton<Database>();
+
+builder.Services.ConfigureFluentMigrator(builder.Configuration);
+
+var app = builder.Build();
+
+var logger = app.Services.GetRequiredService<ILoggerManager>();
+app.ConfigureExceptionHandler(logger);
+
+if (app.Environment.IsProduction())
+	app.UseHsts();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+	ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.MigrateDatabase(logger);
+
+app.Run();
